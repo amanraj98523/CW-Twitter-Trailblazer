454a6ddb803145811c91c05c7ce23b001034db46
Aman Raj <166653226+amanraj98523@users.noreply.github.com>
2024-06-27 17:32:37 +0530

    Add files via upload

---
+++ CW-Twitter-Trailblazers_099-main/assest/js/algo_list.js
@@ -0,0 +1,81 @@
+const algorithms_Data = [
+    {
+        "id": "2183",
+        "label": "Queue",
+        "text": "Operates on a First In, First Out (FIFO) principle",
+        "redirect_page": "queue.html",
+        "image":"banner_1.png",
+        "content_id" : 2183
+    },{
+        "id": "4184",
+        "label": "Stack",
+        "text": "Operates on a Last In, First Out (LIFO) principle",
+        "redirect_page": "stack.html",
+        "image":"banner_2.png",
+        "content_id" : 4184
+    },{
+        "id": "2078",
+        "label": "Bubble Sort",
+        "text": "Bubble Sort Visualiser",
+        "redirect_page": "bubble_sort.html",
+        "image":"banner_7.png",
+        "content_id" : 2078
+    },{
+        "id": "3078",
+        "label": "Selection Sort",
+        "text": "Selection Sort Visualiser",
+        "redirect_page": "selection_sort.html",
+        "image":"banner_3.png",
+        "content_id" : 3078
+    },{
+        "id": "4078",
+        "label": "Merge Sort",
+        "text": "Merge Sort Visualiser",
+        "redirect_page": "merge_sort.html",
+        "image":"banner_6.png",
+        "content_id" : 4078
+    },{
+        "id": "1265",
+        "label": "Sliding Window",
+        "text": "Sliding Window Visualiser",
+        "redirect_page": "sliding_window.html",
+        "image":"banner_5.png",
+        "content_id" : 1265
+    },{
+        "id": "3183",
+        "label": "Binary Search",
+        "text": "Repeatedly dividing the search interval in half.",
+        "redirect_page": "binary_search.html",
+        "image":"banner_4.png",
+        "content_id" : 3183
+
+    },{
+        "id": "5078",
+        "label": "Linked List",
+        "text": "Linked List Visualiser",
+        "redirect_page": "linked_list.html",
+        "image":"banner_8.png",
+        "content_id" : 5078
+    },{
+        "id": "1078",
+        "label": "Quick Sort",
+        "text": "Quick Sort Visualiser",
+        "redirect_page": "quick_sort.html",
+        "image":"banner_4.png",
+        "content_id" : 1078
+    },,{
+        "id": "2265",
+        "label": "Two pointers",
+        "text": "Two Pointer visualizer",
+        "redirect_page": "twopointer.html",
+        "image":"banner_7.png",
+        "content_id" : 2265
+    },{
+        "id": "6078",
+        "label": "Sieve of Eratosthenes",
+        "text": "Sieve of Eratosthenes Visualizer ",
+        "redirect_page": "soe.html",
+        "image":"banner_9.png",
+        "content_id" : 6078
+    }
+];
